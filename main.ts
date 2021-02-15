a4c4c9e6a58dd01e604b169747f7066da0aca107
Hipokondriako <79016081+Hipokondriako@users.noreply.github.com>
2021-02-15 17:18:18 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+function Black () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
+}
+function White () {
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.analogWritePin(AnalogPin.P2, 1023)
+}
+basic.forever(function () {
+    if (input.lightLevel() < 100) {
+        White()
+        basic.clearScreen()
+    } else {
+        Black()
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    }
+})
